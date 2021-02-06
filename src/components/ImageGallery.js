b6985ca2bb7a8bfdb046f2bef43e4ee2a544392e
Justinnn07 <justinv2006@gmail.com>
2021-02-06 09:47:26 +0530

    initial commit :fire:

---
+++ src/components/ImageGallery.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ImageGallery from "react-image-gallery";
+import "../App.css";
+
+const ImageGallerys = () => {
+  const images = [
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+  ];
+
+  return (
+    <div className="imageGallery">
+      <ImageGallery items={images} />
+    </div>
+  );
+};
+
+export default ImageGallerys;
---
+++ src/components/Profile.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Profile.css";
+const Profile = () => {
+  return (
+    <div className="profile">
+      <div>
+        <iframe
+          src="https://tinder-cards.firebaseapp.com"
+          frameborder="0"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
